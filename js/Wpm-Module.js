518dede53f5cbecabb8cd76c8f3be15667211d07
Azamat Soodaev <azamat-98kg@mail.ru>
2019-11-16 18:22:18 +0600

    added timer

--- js/Wpm-Module.js
+++ js/Wpm-Module.js
@@ -3,19 +3,21 @@ const WpmText = (function($) {
   let text;
   let wordId;
   let word;
-  let wordsAmount;
-  let textLength;
+  let noofseconds;
+  let timeInterval;
+  let symbols;
+  let realTimeScore;
+  let max;
+  let started_at;
+  let ended_at;
+
+  // setters
 
   function setText() {
     text = getText().split(" ").map((word, id) => {
       return `<span data-word-id="${id}">${word}</span>`;
-    });
-  } 
-
-  function setWordsAmount() {
-    wordsAmount = $('#sourceText span').length;
-    console.log(wordsAmount);
-  }
+    });  
+  }  
 
   function setUnderlinedWord() {
     word.attr('class', 'underlined');
@@ -33,12 +35,22 @@ const WpmText = (function($) {
     $('input#userText').focus();
   }
 
+  function setEndTime() {
+    ended_at = new Date().getSeconds();
+  }
+
+  // getters
+
   function getText() {
-    let text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+    let text = 'They suspect him of human being.';
     if (!text.length) {
       return '';
     } 
     return text;
+  }
+
+  function getMySpeed() {
+    return Math.round(((symbols / 5 / noofseconds) * 60) * 100) / 100;
   }
 
   function printText() {
@@ -47,6 +59,24 @@ const WpmText = (function($) {
 
   function printTextLength() {
     $('#text-length').html(getText().length + ' symbols');
+  }
+
+  function printCurrentScore() {
+    realTimeScore = setInterval(() => {
+      $('#wpm').html( getMySpeed() + ' wpm' );
+    }, 1000);
+  }
+
+  function printTime() {
+    $('#time').text(Math.floor(noofseconds) + ' seconds');
+  }
+
+  function printSymbols() {
+    $('#symbols').text(symbols);
+  }
+
+  function printWPM() {
+    $('#user_score').text( getMySpeed() + ' wpm' );
   }
 
   function disablePrinting() {
@@ -65,47 +95,75 @@ const WpmText = (function($) {
     word.attr('class', 'invalid-char');
   }
 
+  function startTimer() {
+    started_at = new Date().getSeconds();
+    symbols = 0; 
+    noofseconds = 0;
+    max = 0;
+
+    timeInterval = setInterval(() => { 
+      noofseconds += 0.1; 
+    }, 100); 
+  } 
+
   String.prototype.rtrim = function() {
     let trimmed = this.replace(/\s+$/g, '');
     return trimmed;
   };
 
   return {
-    restart: function() {   
+    restart: function() {  
       setText();
-      printText();
-      setWordsAmount();
+      printText(); 
       printTextLength();
       setWordId();
       setWord();
       setUnderlinedWord();
       setFocusOnInput();
+      startTimer();
+      printCurrentScore();
+    },
+
+    endGame: function () {
+      disablePrinting();
+      clearInput();
+      prevWordCorrect();
+      clearInterval(timeInterval);
+      clearInterval(realTimeScore);
+      printWPM();
+      printSymbols();
+      setEndTime();
+      printTime();
     },
 
     validateText: function(value) { 
       let word_substring = word.text().substr(0, value.length);
       let isEqual = value.rtrim() === word.text();
       let isSpaceKeyPressed = value.length - 1 === word.text().length;
-      let isLastWord = (wordId+1) === wordsAmount;
+      let isLastWord = (wordId+1) === text.length;
 
       if (isSpaceKeyPressed && isEqual) {
         clearInput();
         prevWordCorrect();
         setWord();
         setUnderlinedWord();
+        max = 0;
+        return;
       }
-      else if (isLastWord && isEqual) {
-        disablePrinting();
-        clearInput();
-        prevWordCorrect();
-      }
-      else if (value === word_substring) {
+
+      if (value === word_substring) {
+        if (max < value.length) {
+          max = value.length;
+          symbols++;
+        } 
         setUnderlinedWord();
-      } 
-      else {
-        invalidChar(); 
+      } else {
+        invalidChar();
       }
+
+      if (isLastWord && isEqual) {
+        this.endGame();
+      } 
     }
   };
-
 })(jQuery);
